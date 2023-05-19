c18d940edf833246521b3582c08b38303268b700
Restart-PC <klip.ry@gmail.com>
2023-05-19 20:20:46 +0300

    Поправки на сегодня

--- app/src/main/java/com/example/dkakfpkoaopf/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/dkakfpkoaopf/ui/home/HomeFragment.kt
@@ -1,36 +1,67 @@
 package com.example.dkakfpkoaopf.ui.home
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.dkakfpkoaopf.databinding.FragmentHomeBinding
 
+
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var homeViewModel: HomeViewModel
+    private val layoutList: MutableList<LinearLayout> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        // Add the layouts you want to apply animation to the mutable list
+        layoutList.add(binding.WorkExperience)
+        layoutList.add(binding.InfoMy)
+        layoutList.add(binding.OneOrder)
+        layoutList.add(binding.TwoOrder)
+        layoutList.add(binding.ThreeOrder)
+        layoutList.add(binding.FourOrder)
+
+        // Initialize animation for each layout
+        layoutList.forEach { layout ->
+            homeViewModel.initialize(layout)
+        }
+
+        val scrollView = binding.scrollView4
+        scrollView.viewTreeObserver.addOnScrollChangedListener {
+            val scrollViewHeight = scrollView.height
+            val visibleRect = Rect(0, scrollView.scrollY, scrollView.width, scrollView.scrollY + scrollViewHeight)
+
+            layoutList.forEach { layout ->
+                val containerRect = Rect()
+                layout.getGlobalVisibleRect(containerRect)
+
+                if (Rect.intersects(visibleRect, containerRect)) {
+                    homeViewModel.startAnimation(layout)
+                }
+            }
+        }
 
 
         return root
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/dkakfpkoaopf/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/dkakfpkoaopf/ui/home/HomeViewModel.kt
@@ -1,13 +1,77 @@
 package com.example.dkakfpkoaopf.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.util.Log
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.LinearLayout
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+    private val containerMap: MutableMap<LinearLayout, AnimatorSet> = mutableMapOf()
+    private val animatedLayouts: MutableSet<LinearLayout> = mutableSetOf()
+    fun initialize(layout: LinearLayout) {
+        val container = layout
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        val startAlpha = 0f
+        val endAlpha = 1f
+
+        val appearanceAnimator = ObjectAnimator.ofFloat(container, "alpha", startAlpha, endAlpha)
+        appearanceAnimator.duration = 2500
+        appearanceAnimator.interpolator = AccelerateDecelerateInterpolator()
+
+        val startX = -1500f
+        val endX = 0f
+
+        val movementAnimator = ObjectAnimator.ofFloat(container, "translationX", startX, endX)
+        movementAnimator.duration = 2500
+        movementAnimator.interpolator = AccelerateDecelerateInterpolator()
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(appearanceAnimator, movementAnimator)
+
+        containerMap[container] = animatorSet
+        Log.d("Animation", "Animation initialized for layout: $layout")
     }
-    val text: LiveData<String> = _text
+
+    fun startAnimation(layout: LinearLayout) {
+        if (!animatedLayouts.contains(layout)) {
+            containerMap[layout]?.let { animatorSet ->
+                if (animatorSet.isRunning) {
+                    // Animation is already running, no need to start it again
+                    return
+                }
+
+                val listener = object : Animator.AnimatorListener {
+                    override fun onAnimationStart(animation: Animator) {
+                        layout.visibility = View.VISIBLE
+                    }
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        // Actions after animation ends
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+                        // Actions on animation cancellation
+                    }
+
+                    override fun onAnimationRepeat(animation: Animator) {
+                        // Actions on animation repetition
+                    }
+                }
+                animatorSet.addListener(listener)
+                layout.visibility = View.INVISIBLE
+                animatorSet.start()
+                animatedLayouts.add(layout) // Add the layout to the list of animated layouts
+
+
+
+            }
+        }
+    }
+
+
 }
+
